e6ea65e5d41cde05293c3bc57cfd1948b7f3b730
Elliott Jones <66728036+elliottejones@users.noreply.github.com>
2023-09-15 10:26:28 +0100

    0.1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,74 @@ namespace Car_Park
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.Title = "Bob's Car Park";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string reg = "";
+
+
+            void write(string str)
+            {
+                Console.WriteLine(str);
+            }
+
+
+            bool entry()
+            {
+                Console.Clear();
+                write("############################");
+                write("");
+                write("Welcome To Bob's Car Park");
+                write("");
+                write("Enter Your Reg Number");
+                write("");
+                write("############################");
+                write("");
+                while (true)
+                {
+                    reg = Console.ReadLine();
+
+                    if (reg == "")
+                    {
+                        write("Please Enter A Reg Number");
+                        write("");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.Clear() ;
+                write("############################");
+                write("");
+                write("You Entered: "+reg+". Is that correct? (y/n)");
+                write("");
+                write("############################");
+                write("");
+
+                string confirm = Console.ReadLine();
+
+                if (confirm == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            while (true)
+            {
+                if (entry() == true)
+                {
+                    break;
+                }
+                
+            }
+
         }
     }
 }
